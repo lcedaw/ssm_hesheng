5ef7001182617e024a0b8eec6def606820c1f9dd
ljz <190289015@qq.com>
2019-12-10 14:25:13 +0800

    完成servlet接口访问

---
+++ meq/src/main/java/com/lc/meq/web/SysUsersController.java
@@ -0,0 +1,31 @@
+package com.lc.meq.web;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSONObject;
+import com.lc.meq.dao.SysUsersDao;
+import com.lc.meq.entity.SysUsers;
+
+@Controller
+@RequestMapping("/sysUsers") // url:/模块/资源/{id}/细分 /seckill/list
+public class SysUsersController {
+
+	@Autowired
+	private SysUsersDao sysUsersDao;
+	
+	@RequestMapping(value = "login",method = RequestMethod.GET)
+	@ResponseBody
+	public String login(HttpServletRequest request) {
+		String uid = "8f02bda8-e22d-4ea2-b1cb-0873d1be8b6e";
+		SysUsers sysUsers = sysUsersDao.queryById(uid);
+		return sysUsers.jsonString(sysUsers);
+	}
+	
+}
