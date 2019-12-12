3fb9cf8e9031915250764a8ea516c7424e41eb5b
ljz <190289015@qq.com>
2019-12-12 15:59:20 +0800

    增加@param参数，让mybatis识别

--- meq/src/main/java/com/lc/meq/dao/SysUsersDao.java
+++ meq/src/main/java/com/lc/meq/dao/SysUsersDao.java
@@ -2,6 +2,8 @@ package com.lc.meq.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.lc.meq.entity.SysUsers;
 
 public interface SysUsersDao {
@@ -20,4 +22,13 @@ public interface SysUsersDao {
 	 * @return
 	 */
 	List<SysUsers> queryUsersAll();
+	
+	/**
+	 * 通过账号和名称查询用户
+	 * 
+	 * @param userCode
+	 * @param userName
+	 * @return
+	 */
+	SysUsers getSysUsers(@Param("userCode") String userCode ,@Param("userName") String userName);
 }
