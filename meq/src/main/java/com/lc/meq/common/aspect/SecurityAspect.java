1eb0e161d1f9ef004cc1c7087f850bec168a2918
ljz <190289015@qq.com>
2019-12-19 10:21:37 +0800

    增加aspsj jar包

---
+++ meq/src/main/java/com/lc/meq/common/aspect/SecurityAspect.java
@@ -0,0 +1,13 @@
+package com.lc.meq.common.aspect;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * token有效性判断切面
+ * @author ljz
+ * @date 2019-12-18
+ */
+@Component
+public class SecurityAspect {
+
+}
